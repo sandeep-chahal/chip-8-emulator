04e6416ee8aff044d87580d3c9228445496af15d
sandeep chahal <Sandeepchahal573@gmail.com>
2021-01-02 14:50:57 +0530

    added keyboard

--- src/js/chip8.ts
+++ src/js/chip8.ts
@@ -1,5 +1,7 @@
 import Monitor from "./monitor";
 import Speaker from "./speaker";
+import Keyboard from "./keyboard";
 
 const monitor = new Monitor(document.querySelector("canvas"), 10);
 const speaker = new Speaker();
+const keyboard = new Keyboard();
